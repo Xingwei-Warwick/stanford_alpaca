33566d2c95ea8f90b5db8c5a968885eb05af0e49
Lastdier <a499340016@gmail.com>
2023-08-21 14:53:03 +0100

    update

--- finetune_script_debug.sh
+++ finetune_script_debug.sh
@@ -1,4 +1,8 @@
 
+curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | sudo bash
+
+sudo apt-get install git-lfs
+
 git lfs install
 
 git clone https://huggingface.co/tatsu-lab/alpaca-7b-wdiff
